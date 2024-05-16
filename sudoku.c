92bb1d702a317ea96b459ad7a1b46d8dc41ba3f9
Your Name <you@example.com>
2024-05-16 16:08:22 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -1,78 +1,71 @@
+#include "list.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "list.h"
 
-typedef struct{
-   int sudo[9][9];
-}Node;
+typedef struct {
+  int sudo[9][9];
+} Node;
 
-Node* createNode(){
-  Node* n=(Node*) malloc(sizeof(Node));
+Node *createNode() {
+  Node *n = (Node *)malloc(sizeof(Node));
   return n;
 }
 
-Node* copy(Node* n){
-    Node* new=(Node*) malloc(sizeof(Node));
-    *new = *n;
-    return new;
+Node *copy(Node *n) {
+  Node *new = (Node *)malloc(sizeof(Node));
+  *new = *n;
+  return new;
 }
 
-Node* read_file (char* file_name){
-  Node* n = createNode();
-  FILE* file = fopen (file_name, "r");
-  int i,j;
-  for(i=0;i<9;i++){
-       for(j=0;j<9;j++){
-          if(!fscanf (file, "%d", &n->sudo[i][j]))
-            printf("failed to read data!");
-       }
+Node *read_file(char *file_name) {
+  Node *n = createNode();
+  FILE *file = fopen(file_name, "r");
+  int i, j;
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++) {
+      if (!fscanf(file, "%d", &n->sudo[i][j]))
+        printf("failed to read data!");
+    }
   }
 
-  fclose (file);
+  fclose(file);
   return n;
 }
 
-void print_node(Node* n){
-    int i,j;
-    for(i=0;i<9;i++){
-       for(j=0;j<9;j++)
-          printf("%d ", n->sudo[i][j]);
-       printf("\n");
-    }
+void print_node(Node *n) {
+  int i, j;
+  for (i = 0; i < 9; i++) {
+    for (j = 0; j < 9; j++)
+      printf("%d ", n->sudo[i][j]);
     printf("\n");
+  }
+  printf("\n");
 }
 
-int is_valid(Node* n){
-   
+int is_valid(Node *n) { return 1; }
 
-    return 1;
-}
-
-List* get_adj_nodes(Node* n) {
-    List* adj_nodes = createList();
-    for (int i = 0; i < 9; i++) {
-        for (int j = 0; j < 9; j++) {
-            if (n->sudo[i][j] == 0) {
-                for (int num = 1; num <= 9; num++) {
-                    Node* new_node = copy(n);
-                    new_node->sudo[i][j] = num;
-                    pushBack(adj_nodes, new_node);
-                }
-
-                break;
-            }
+List *get_adj_nodes(Node *n) {
+  List *adj_nodes = createList();
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      if (n->sudo[i][j] == 0) {
+        for (int num = 1; num <= 9; num++) {
+          Node *new_node = copy(n);
+          new_node->sudo[i][j] = num;
+          if (is_valid)
+            
+pushBack(adj_nodes, new_node);
         }
+
+        break;
+      }
     }
+  }
 
-    return adj_nodes;
+  return adj_nodes;
 }
 
-
-Node* DFS(Node* initial, int* cont){
-  return NULL;
-}
-
-
+Node *DFS(Node *initial, int *cont) { return NULL; }
 
 /*
 int main( int argc, char *argv[] ){
