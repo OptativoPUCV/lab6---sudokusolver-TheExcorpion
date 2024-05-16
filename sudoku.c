7b6f2b439600a8bf3cb438d35e2ba1f788990a40
Your Name <you@example.com>
2024-05-16 16:12:46 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -42,74 +42,42 @@ void print_node(Node *n) {
   printf("\n");
 }
 
-int is_valid(Node *n) {
-  // Check each row
+int is_valid(Node* n) {
+  int row[9][9] = {0}, col[9][9] = {0}, box[9][9] = {0};
   for (int i = 0; i < 9; i++) {
-    int seen[10] = {0}; // Array to keep track of numbers seen in this row
-    for (int j = 0; j < 9; j++) {
-      int num = n->sudo[i][j];
-      if (num != 0 && seen[num]) {
-        printf("Invalid row: %d\n", i);
-        return 0; // If the number is repeated in this row, the Sudoku is invalid
-      }
-      seen[num] = 1;
-    }
-  }
-
-  // Check each column
-  for (int j = 0; j < 9; j++) {
-    int seen[10] = {0}; // Array to keep track of numbers seen in this column
-    for (int i = 0; i < 9; i++) {
-      int num = n->sudo[i][j];
-      if (num != 0 && seen[num]) {
-        printf("Invalid column: %d\n", j);
-        return 0; // If the number is repeated in this column, the Sudoku is invalid
-      }
-      seen[num] = 1;
-    }
-  }
-
-  // Check each 3x3 subgrid
-  for (int i = 0; i < 9; i += 3) {
-    for (int j = 0; j < 9; j += 3) {
-      int seen[10] = {0}; // Array to keep track of numbers seen in this subgrid
-      for (int di = 0; di < 3; di++) {
-        for (int dj = 0; dj < 3; dj++) {
-          int num = n->sudo[i + di][j + dj];
-          if (num != 0 && seen[num]) {
-            printf("Invalid subgrid: %d %d\n", i, j);
-            return 0; // If the number is repeated in this subgrid, the Sudoku is invalid
+      for (int j = 0; j < 9; j++) {
+          if (n->sudo[i][j] != 0) {
+              int num = n->sudo[i][j] - 1;
+              int k = (i / 3) * 3 + j / 3;
+              if (row[i][num] || col[j][num] || box[k][num])
+                  return 0;
+              row[i][num] = col[j][num] = box[k][num] = 1;
           }
-          seen[num] = 1;
-        }
       }
-    }
   }
-
-  return 1; // If all checks pass, the Sudoku is valid
+  return 1;
 }
 
 
-List *get_adj_nodes(Node *n) {
-  List *adj_nodes = createList();
+List* get_adj_nodes(Node* n) {
+  List* list = createList();
   for (int i = 0; i < 9; i++) {
-    for (int j = 0; j < 9; j++) {
-      if (n->sudo[i][j] == 0) {
-        for (int num = 1; num <= 9; num++) {
-          Node *new_node = copy(n);
-          new_node->sudo[i][j] = num;
-          if (is_valid(new_node)) { // Call is_valid and check its return value
-            pushBack(adj_nodes, new_node);
-          } else {
-            printf("Invalid node generated\n");
-            free(new_node); // Free memory if the node is not valid
+      for (int j = 0; j < 9; j++) {
+          if (n->sudo[i][j] == 0) { 
+              for (int num = 1; num <= 9; num++) {
+                  Node* new_node = copy(n);
+                  new_node->sudo[i][j] = num;
+                  if (is_valid(new_node)) {
+                      pushBack(list, new_node);
+                  } else {
+                      free(new_node); 
+                  }
+              }
+              return list; 
           }
-        }
-        return adj_nodes; // Return the list of adjacent nodes immediately after adding nodes
       }
-    }
   }
-  return adj_nodes;
+  return list; 
 }
 
 
