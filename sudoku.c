c02972e790b74b3dbae38cab41d76ce3f4053409
Your Name <you@example.com>
2024-05-16 16:09:55 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -42,7 +42,49 @@ void print_node(Node *n) {
   printf("\n");
 }
 
-int is_valid(Node *n) { return 1; }
+int is_valid(Node *n) {
+  // Check each row
+  for (int i = 0; i < 9; i++) {
+    int seen[10] = {0}; // Array to keep track of numbers seen in this row
+    for (int j = 0; j < 9; j++) {
+      int num = n->sudo[i][j];
+      if (num != 0 && seen[num]) {
+        return 0; // If the number is repeated in this row, the Sudoku is invalid
+      }
+      seen[num] = 1;
+    }
+  }
+
+  // Check each column
+  for (int j = 0; j < 9; j++) {
+    int seen[10] = {0}; // Array to keep track of numbers seen in this column
+    for (int i = 0; i < 9; i++) {
+      int num = n->sudo[i][j];
+      if (num != 0 && seen[num]) {
+        return 0; // If the number is repeated in this column, the Sudoku is invalid
+      }
+      seen[num] = 1;
+    }
+  }
+
+  // Check each 3x3 subgrid
+  for (int i = 0; i < 9; i += 3) {
+    for (int j = 0; j < 9; j += 3) {
+      int seen[10] = {0}; // Array to keep track of numbers seen in this subgrid
+      for (int di = 0; di < 3; di++) {
+        for (int dj = 0; dj < 3; dj++) {
+          int num = n->sudo[i + di][j + dj];
+          if (num != 0 && seen[num]) {
+            return 0; // If the number is repeated in this subgrid, the Sudoku is invalid
+          }
+          seen[num] = 1;
+        }
+      }
+    }
+  }
+
+  return 1; // If all checks pass, the Sudoku is valid
+}
 
 List *get_adj_nodes(Node *n) {
   List *adj_nodes = createList();
@@ -52,15 +94,19 @@ List *get_adj_nodes(Node *n) {
         for (int num = 1; num <= 9; num++) {
           Node *new_node = copy(n);
           new_node->sudo[i][j] = num;
-          if (is_valid)
-            
-pushBack(adj_nodes, new_node);
+          if (is_valid(new_node)) { // Call is_valid and check its return value
+            pushBack(adj_nodes, new_node);
+          } else {
+            free(new_node); // Free memory if the node is not valid
+          }
         }
-
-        break;
+        return adj_nodes; // Return the list of adjacent nodes immediately after adding nodes
       }
     }
   }
+  return adj_nodes;
+}
+
 
   return adj_nodes;
 }
